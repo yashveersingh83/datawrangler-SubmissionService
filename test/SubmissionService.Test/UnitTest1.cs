bcd9efe4a93a38c508aa4aa614e59ca62de60998
Yashveer <yashveersingh83@gmail.com>
2025-04-24 18:44:49 +0200

    Added a unit test project so that we can demonstrate test runs as well

---
+++ test/SubmissionService.Test/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿namespace SubmissionService.Test
+{
+    public class SampleTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Test2()
+        {
+            Assert.Pass();
+        }
+    }
+}
